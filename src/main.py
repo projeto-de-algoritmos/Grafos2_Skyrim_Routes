b2421f92be8d3826e106fac3b6bfd0e3befe4645
Matheus Gabrie <matheuscoding@gmail.com>
2021-08-20 13:13:24 -0300

    Adicionando mudanças na home do projeto
    Co-authored-by: Pedrok99 <pedrohlima20@gmail.com>

--- src/main.py
+++ src/main.py
@@ -4,6 +4,8 @@ from flask import Flask, json, render_template,request
 from graphclass import Graph
 from dijkstra import shortest_path
 
+app = Flask(__name__,template_folder='../front')
+
 df = pd.read_csv('./static/NodesSkyrim.csv')
 edges = []
 
@@ -13,6 +15,26 @@ for i, row in df.iterrows():
 
 grafo = Graph(edges=edges, directed=False)
 
-path = shortest_path(grafo, 'Solitude', 'Dushnikh Yal')
+@app.route('/', methods=["GET","POST"])
+def index():
+    if request.method == "GET":
+        vertices = grafo.get_vertices()
+        return render_template("index.html",vertices=vertices)
+    else:
+        pass
+        # target = str(request.form.get('target')).replace("_"," ")
+        # dependent = get_dependents(grafo, target)
+        # # dependent[0] = [i for i in dependent[0] if i != '']
+        #
+        # if '' in dependent[0]:
+        #     dependent[0] = ['Nenhuma matéria' for i in dependent[0] if i == '']
+        #
+        # if '' in dependent[1]:
+        #     for i,value in enumerate(dependent[1]):
+        #         if value == '':
+        #             delete_element(dependent[1],i )
+        #
+        # dependencie = get_dependencies(grafo, target)
+        # return render_template('list.html', target=target, dependent=dependent, dependencies=dependencie)
 
-print(path)
+app.run(debug=True)
